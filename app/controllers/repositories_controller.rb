44b3f88bc232c7e1629dab7d735817429e2023ba
James Randolph <james.randolph2016@gmail.com>
2018-07-12 20:51:27 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,5 +8,7 @@ class RepositoriesController < ApplicationController
     client_id = "854c39df2f866cd3f15c"
     secret = "37b3f474525a6f0608ccba69d67a28c37872a495"
     
+    resp = Faraday.get("")
+    
   end
 end
